7e5012c890eaa801d9c187c89584248db7231de9
Mu Lu <murphytalk@gmail.com>
2024-04-29 01:43:01 +0900

    nvim: switched to lazy-nvim

--- .config/nvim/lua/my-nvim/init.lua
+++ .config/nvim/lua/my-nvim/init.lua
@@ -1,2 +1,75 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- lazy-nvim requires leader key be setup first
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+require("lazy").setup({
+    {
+      "craftzdog/solarized-osaka.nvim",
+      name = "solarized-osaka",
+      lazy = false, -- make sure we load this during startup if it is your main colorscheme
+      priority = 1000, -- make sure to load this before all the other start plugins
+    },
+    {
+      "olimorris/onedarkpro.nvim",
+      lazy = true,
+      name = "onedark"
+    },
+    {
+      'nvim-telescope/telescope.nvim', tag = '0.1.6',
+      -- or                              , branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+      'nvim-treesitter/nvim-treesitter',
+      build = ":TSUpdate"
+    },
+    {
+      'mbbill/undotree'
+    },
+    {
+      'tpope/vim-fugitive'
+    },
+    {
+      'duane9/nvim-rg'
+    },
+    {
+      'yorickpeterse/nvim-window'
+    },
+    --
+    -- LSP stuff
+    {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v3.x'
+    },
+    {'neovim/nvim-lspconfig'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-nvim-lua'},
+
+    -- Snippets
+    {'L3MON4D3/LuaSnip'},
+    {'rafamadriz/friendly-snippets'}
+}, {})
+
+
 require("my-nvim.remap")
 require("my-nvim.setup")
