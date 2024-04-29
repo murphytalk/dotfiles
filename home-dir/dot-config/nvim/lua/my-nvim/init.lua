c995500d3f78973b8190ca39ecb0adc71410409e
Mu Lu <murphytalk@gmail.com>
2024-04-29 18:46:23 +0900

    1) remove lazy-lock 2) added lualine

--- home-dir/dot-config/nvim/lua/my-nvim/init.lua
+++ home-dir/dot-config/nvim/lua/my-nvim/init.lua
@@ -17,42 +17,46 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 require("lazy").setup({
     {
-      "craftzdog/solarized-osaka.nvim",
-      name = "solarized-osaka",
-      lazy = false, -- make sure we load this during startup if it is your main colorscheme
-      priority = 1000, -- make sure to load this before all the other start plugins
+        "craftzdog/solarized-osaka.nvim",
+        name = "solarized-osaka",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
     },
     {
-      "olimorris/onedarkpro.nvim",
-      lazy = true,
-      name = "onedark"
+        "olimorris/onedarkpro.nvim",
+        lazy = true,
+        name = "onedark"
     },
     {
-      'nvim-telescope/telescope.nvim', tag = '0.1.6',
-      -- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
     },
     {
-      'nvim-treesitter/nvim-treesitter',
-      build = ":TSUpdate"
+        'nvim-treesitter/nvim-treesitter',
+        build = ":TSUpdate"
     },
     {
-      'mbbill/undotree'
+        'mbbill/undotree'
     },
     {
-      'tpope/vim-fugitive'
+        'tpope/vim-fugitive'
     },
     {
-      'duane9/nvim-rg'
+        'duane9/nvim-rg'
     },
     {
-      'yorickpeterse/nvim-window'
+        'yorickpeterse/nvim-window'
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
     },
     --
     -- LSP stuff
     {
-      'VonHeikemen/lsp-zero.nvim',
-      branch = 'v3.x'
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x'
     },
     {'neovim/nvim-lspconfig'},
     {'williamboman/mason.nvim'},
