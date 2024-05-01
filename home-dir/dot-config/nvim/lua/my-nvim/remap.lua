21273544ea57f5a724987e933e7ee99c6b7d6c6c
Mu Lu <murphytalk@gmail.com>
2024-05-01 23:44:49 +0900

    C-x-0 to close current buffer

--- home-dir/dot-config/nvim/lua/my-nvim/remap.lua
+++ home-dir/dot-config/nvim/lua/my-nvim/remap.lua
@@ -2,6 +2,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- old emacs habit
 vim.api.nvim_set_keymap('n', '<C-x><C-f>', ':edit ', { noremap = true, silent = false })
+vim.api.nvim_set_keymap('n', '<C-x>0', ':bd<CR>', { noremap = true, silent = false })
 vim.api.nvim_set_keymap('n', '<C-x>1', ':only<CR>', { noremap = true, silent = false })
 vim.api.nvim_set_keymap('n', '<C-x>2', ':split<CR>', { noremap = true, silent = false })
 vim.api.nvim_set_keymap('n', '<C-x>3', ':vsplit<CR>', { noremap = true, silent = false })
