5c03143820a24ff301c11137c1e6a82f18c9bb73
Mu Lu <murphytalk@gmail.com>
2024-05-04 00:16:52 +0900

    clean up

--- home-dir/dot-config/nvim/lua/my-nvim/plugins/nvim-dap.lua
+++ home-dir/dot-config/nvim/lua/my-nvim/plugins/nvim-dap.lua
@@ -50,52 +50,6 @@ return {
             return py
         end
         vim.keymap.set('n', '<Leader>dt', dap.toggle_breakpoint, {})
---[[
-        dap.adapters.python = function(cb, config)
-            if config.request == 'attach' then
-                ---@diagnostic disable-next-line: undefined-field
-                local port = (config.connect or config).port
-                ---@diagnostic disable-next-line: undefined-field
-                local host = (config.connect or config).host or '127.0.0.1'
-                cb({
-                    type = 'server',
-                    port = assert(port, '`connect.port` is required for a python `attach` configuration'),
-                    host = host,
-                    options = {
-                        source_filetype = 'python',
-                    },
-                })
-            else
-                cb({
-                    type = 'executable',
-                    command = find_venv() .. '/bin/python',
-                    args = { '-m', 'debugpy.adapter' },
-                    options = {
-                        source_filetype = 'python',
-                    },
-                })
-            end
-        end
-
-        vim.keymap.set('n', '<Leader>dc', function ()
-            local python = find_venv() .. '/bin/python'
-            print('use python at ' .. python)
-            dap.configurations.python = {
-                {
-                    -- The first three options are required by nvim-dap
-                    type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
-                    request = 'launch';
-                    name = "Launch file";
-
-                    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
-
-                    program = "${file}"; -- This configuration will launch the current file if used.
-                    pythonPath = python
-                },
-            }
-            dap.continue()
-        end, {})
-]]--
         vim.keymap.set('n', '<Leader>dc', dap.continue, {})
     end
 }
