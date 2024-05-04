5b1b6654833b213654b096efde18057cfc0a1df9
Mu Lu <murphytalk@gmail.com>
2024-05-04 22:46:17 +0900

    lsp: ensure lua

--- home-dir/dot-config/nvim/after/plugin/lsp.lua
+++ home-dir/dot-config/nvim/after/plugin/lsp.lua
@@ -11,7 +11,7 @@ end)
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'clangd', 'pyright'},
+  ensure_installed = {'clangd', 'pyright','lua_ls'},
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
--- home-dir/dot-config/nvim/lua/my-nvim/plugins/telescope.lua
+++ home-dir/dot-config/nvim/lua/my-nvim/plugins/telescope.lua
@@ -2,19 +2,74 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.6',
     -- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        {
+            -- If encountering errors, see telescope-fzf-native README for installation instructions
+            'nvim-telescope/telescope-fzf-native.nvim',
+            -- `build` is used to run some command when the plugin is installed/updated.
+            -- This is only run then, not every time Neovim starts up.
+            build = 'make',
+
+            -- `cond` is a condition used to determine whether this plugin should be
+            -- installed and loaded.
+            cond = function()
+                return vim.fn.executable 'make' == 1
+            end,
+        },
+        { 'nvim-telescope/telescope-ui-select.nvim' },
+        -- Useful for getting pretty icons, but requires a Nerd Font.
+        { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+    },
     config = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fG', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>f.', builtin.oldfiles, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
-        vim.keymap.set('n', '<leader>ps', function()
-            -- needs to install ripgrep
-            builtin.grep_string({ search = vim.fn.input("Grep > ")})
-        end)
-    end
+      -- [[ Configure Telescope ]]
+      -- See `:help telescope` and `:help telescope.setup()`
+      local tel= require('telescope')
+      tel.setup {
+        -- You can put your default mappings / updates / etc. in here
+        --  All the info you're looking for is in `:help telescope.setup()`
+        --
+        -- defaults = {
+        --   mappings = {
+        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+        --   },
+        -- },
+        -- pickers = {}
+        extensions = {
+          ['ui-select'] = {
+            require('telescope.themes').get_dropdown(),
+          },
+        },
+      }
+      -- Enable Telescope extensions if they are installed
+      pcall(tel.load_extension, 'fzf')
+      pcall(tel.load_extension, 'ui-select')
+
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles'})
+      vim.keymap.set('n', '<leader>fG', builtin.git_files, { desc = '[F]ind [G]it files'})
+      vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[F]ind [K]eymaps' })
+      vim.keymap.set('n', '<leader>f.', builtin.oldfiles, {desc = '[F]ind Recent Files ("." for repeat)'})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep'})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
+      vim.keymap.set('n', '<leader>ps', function()
+          -- needs to install ripgrep
+          builtin.grep_string({ search = vim.fn.input("Grep > ")})
+      end)
+
+      -- Slightly advanced example of overriding default behavior and theme
+      vim.keymap.set('n', '<leader>/', function()
+        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+          winblend = 10,
+          previewer = false,
+        })
+      end, { desc = '[/] Fuzzily search in current buffer' })
+
+      -- Shortcut for searching your Neovim configuration files
+      vim.keymap.set('n', '<leader>fn', function()
+        builtin.find_files { cwd = vim.fn.stdpath 'config' }
+      end, { desc = '[F]ind [N]eovim files' })
+  end
 }
