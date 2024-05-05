be825ccf9ce99cbebd00e1b1f501845dd74cb6ce
Mu Lu <murphytalk@gmail.com>
2024-05-05 21:50:01 +0900

    default colr: tokyonight

--- home-dir/dot-config/nvim/lua/plugins/color.lua
+++ home-dir/dot-config/nvim/lua/plugins/color.lua
@@ -1,17 +1,24 @@
 return {
   {
+    "folke/tokyonight.nvim",
+    cond = not vim.g.vscode,
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd.colorscheme("tokyonight")
+    end,
+  },
+  {
     "craftzdog/solarized-osaka.nvim",
     cond = not vim.g.vscode,
     name = "solarized-osaka",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      vim.cmd.colorscheme("solarized-osaka")
-    end,
+    lazy = true,
   },
   {
     "olimorris/onedarkpro.nvim",
     lazy = true,
+    cond = not vim.g.vscode,
     name = "onedark",
   },
 }
