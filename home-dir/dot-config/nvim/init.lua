89fae3725823aa0ac0aa8d0cf1d734e6ab31a4d6
Mu Lu <murphytalk@gmail.com>
2024-05-05 17:08:32 +0900

    common basis setup

--- home-dir/dot-config/nvim/init.lua
+++ home-dir/dot-config/nvim/init.lua
@@ -28,6 +28,16 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 require("lazy").setup("plugins")
 
+-- shared options
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
 -- Neovim configuration
 if vim.g.vscode then
   require("vscode")
--- home-dir/dot-config/nvim/lua/my-nvim/setup.lua
+++ home-dir/dot-config/nvim/lua/my-nvim/setup.lua
@@ -2,15 +2,6 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
-
 vim.opt.swapfile = false
 vim.opt.backup = false
 if vim.loop.os_uname().sysname == "Linux" then
