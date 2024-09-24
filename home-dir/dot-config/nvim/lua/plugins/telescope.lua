01846f1a28e5474779f008de9bd8e9229ba2c377
Mu Lu <murphytalk@gmail.com>
2024-09-24 21:13:36 +0900

    del buf

--- home-dir/dot-config/nvim/lua/plugins/telescope.lua
+++ home-dir/dot-config/nvim/lua/plugins/telescope.lua
@@ -31,11 +31,14 @@ return {
       -- You can put your default mappings / updates / etc. in here
       --  All the info you're looking for is in `:help telescope.setup()`
       --
-      -- defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      -- },
+      defaults = {
+        mappings = {
+          i = {
+            --['<c-enter>'] = 'to_fuzzy_refine' ,
+            ["<C-d>"] = "delete_buffer",
+          },
+        },
+      },
       -- pickers = {}
       extensions = {
         ["ui-select"] = {
