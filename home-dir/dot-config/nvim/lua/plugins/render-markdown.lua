a11bd166b43d0f238f20107d946f94056946cb59
Mu Lu <murphytalk@gmail.com>
2024-09-24 21:13:18 +0900

    render markdown table

--- home-dir/dot-config/nvim/lua/plugins/render-markdown.lua
+++ home-dir/dot-config/nvim/lua/plugins/render-markdown.lua
@@ -4,4 +4,45 @@ return {
   --dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+  config = function()
+    require("render-markdown").setup({
+      pipe_table = {
+        -- Turn on / off pipe table rendering
+        enabled = true,
+        -- Pre configured settings largely for setting table border easier
+        --  heavy:  use thicker border characters
+        --  double: use double line border characters
+        --  round:  use round border corners
+        --  none:   does nothing
+        preset = "round",
+        -- Determines how the table as a whole is rendered:
+        --  none:   disables all rendering
+        --  normal: applies the 'cell' style rendering to each row of the table
+        --  full:   normal + a top & bottom line that fill out the table when lengths match
+        style = "full",
+        -- Determines how individual cells of a table are rendered:
+        --  overlay: writes completely over the table, removing conceal behavior and highlights
+        --  raw:     replaces only the '|' characters in each row, leaving the cells unmodified
+        --  padded:  raw + cells are padded with inline extmarks to make up for any concealed text
+        cell = "padded",
+        -- Gets placed in delimiter row for each column, position is based on alignmnet
+        alignment_indicator = "━",
+        -- Characters used to replace table border
+        -- Correspond to top(3), delimiter(3), bottom(3), vertical, & horizontal
+        -- stylua: ignore
+        border = {
+            '┌', '┬', '┐',
+            '├', '┼', '┤',
+            '└', '┴', '┘',
+            '│', '─',
+        },
+        -- Highlight for table heading, delimiter, and the line above
+        head = "RenderMarkdownTableHead",
+        -- Highlight for everything else, main table rows and the line below
+        row = "RenderMarkdownTableRow",
+        -- Highlight for inline padding used to add back concealed space
+        filler = "RenderMarkdownTableFill",
+      },
+    })
+  end,
 }
