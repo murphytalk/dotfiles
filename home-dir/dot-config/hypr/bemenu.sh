31c5037c64542323624a887fcb8250c49cc94919
Mu Lu <murphytalk@gmail.com>
2025-11-25 21:04:00 +0900

    hyperland + bemenu

---
+++ home-dir/dot-config/hypr/bemenu.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+bemenu-run -c -l 20 -W 0.5  \
+--fb "#303446" --ff "#c6d0f5" --nb "#303446" --nf "#c6d0f5" --tb "#303446" --hb "#303446" --tf "#e78284" --hf "#e5c890" --af "#c6d0f5" --ab "#303446"
+# Frappé
+
+# Latte
+#               --fb "#eff1f5" --ff "#4c4f69" --nb "#eff1f5" --nf "#4c4f69" \
+#               --tb "#eff1f5" --hb "#eff1f5" --tf "#d20f39" --hf "#df8e1d" \
+#               --af "#4c4f69" --ab "#eff1f5"
+
