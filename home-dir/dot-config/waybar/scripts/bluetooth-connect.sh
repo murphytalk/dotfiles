d824f822dca9cf10fe2deedf10e7a689b5906b70
Mu Lu <murphytalk@gmail.com>
2025-12-13 22:25:49 +0900

    fine tuning waybar styles

--- home-dir/dot-config/waybar/scripts/bluetooth-connect.sh
+++ home-dir/dot-config/waybar/scripts/bluetooth-connect.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 toggle=$1
-DEVICE="00:02:3C:29:3C:98"   # Createive D200
+DEVICE="00:02:3C:29:3C:98" # Createive D200
 
-on_off=$(bluetoothctl info $DEVICE | grep 'Connected:'|sed 's/.*C.*: *\(.*\)$/\1/g')
+on_off=$(bluetoothctl info $DEVICE | grep 'Connected:' | sed 's/.*C.*: *\(.*\)$/\1/g')
 if [ "$on_off" = "no" ]; then
-    cmd=connect
+  cmd=connect
 else
-    cmd=disconnect
+  cmd=disconnect
 fi
 
 if [ ! -z "$toggle" ]; then
-    notify-send "Bluetooth Speaker" "$cmd"ting
-    bluetoothctl $cmd $DEVICE > /dev/null
+  notify-send "Bluetooth Speaker" "$cmd"ting
+  bluetoothctl $cmd $DEVICE >/dev/null
 fi
 
-on_off=$(bluetoothctl info $DEVICE | grep 'Connected:'|sed 's/.*C.*: *\(.*\)$/\1/g')
+on_off=$(bluetoothctl info $DEVICE | grep 'Connected:' | sed 's/.*C.*: *\(.*\)$/\1/g')
 if [ "$on_off" = "no" ]; then
-    if [ ! -z "$toggle" ]; then
-        notify-send "Bluetooth Speaker" " Disconnected"
-    fi
-    echo " "
+  if [ ! -z "$toggle" ]; then
+    notify-send "Bluetooth Speaker" " Disconnected"
+  fi
+  echo "󰓄"
 else
-    if [ ! -z "$toggle" ]; then
-        notify-send "Bluetooth Speaker" " Connected"
-    fi
-    echo " "
+  if [ ! -z "$toggle" ]; then
+    notify-send "Bluetooth Speaker" " Connected"
+  fi
+  echo "󰦢"
 fi
-
--- home-dir/dot-config/waybar/scripts/bluetooth-toggle.sh
+++ home-dir/dot-config/waybar/scripts/bluetooth-toggle.sh
@@ -2,14 +2,13 @@
 toggle=$1
 status=$(bluetoothctl show | grep "Powered" | awk '{print $2}')
 if [ "$status" = "yes" ]; then
-    if [ ! -z "$toggle" ]; then
-        bluetoothctl power off
-    fi
-    echo ""
+  if [ ! -z "$toggle" ]; then
+    bluetoothctl power off
+  fi
+  echo ""
 else
-    if [ ! -z "$toggle" ]; then
-        bluetoothctl power on
-    fi
-    echo ""
+  if [ ! -z "$toggle" ]; then
+    bluetoothctl power on
+  fi
+  echo "󰂲"
 fi
-
